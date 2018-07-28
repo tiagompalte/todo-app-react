63c90f9dfb39cfa6d59b5c61a078981f4673485f
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-07-28 14:23:41 -0300

    Frontend react with redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'/>
+    <h2>Nossa história</h2>
+    <p>Encare problemas como oportunidades. Não perde tempo com bullshit. Viva em busca da masterização e do profissionalismo, every f*ing day.</p>
+    <h2>Missão e visão</h2>
+    <p>Bora pra action. O inconformismo é o combustível da alta performance. Menos desculpas e mais action.</p>
+    <h2>Imprensa</h2>
+    <p>Você tá realmente obcecado pelos seus sonhos? Transforme o seu lifestyle. Menos desculpas e mais action.</p>
+  </div>
+)
